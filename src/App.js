34bc41802f6f0b936be3e1d0556b30ca5055107b
John Doe <johndoe@example.com>
2025-03-15 16:19:23 +0300

    New project from temlate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "ract";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Template</h1>
+      <p>Start template for React-projects</p>
+    </div>
+  );
+}
+
+export default App;
