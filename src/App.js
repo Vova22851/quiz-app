77a1766dfff3c4267beee3b4ce73e33630a64534
John Doe <johndoe@example.com>
2025-03-22 17:36:21 +0300

    new text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,75 @@
 import './App.css';
-import React from "react";
+import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Question from './Components/Question';
+import qBank from './Components/QuestionBank';
+import Score from './Components/Score';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>React Template</h1>
-      <p>Start template for React-projects</p>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionBank: qBank,
+      currentQuestion: 0,
+      selectedOption: "",
+      score: 0,
+      quizEnd: false,
+    }
+  }
+
+  handleOptionChange = (e) => {
+    this.setState({ selectedOption: e.target.value});
+  }
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    this. chrckAnswer();
+    this.handleNextQuestion();
+
+  }
+
+  handleNextQuestion = () => {
+    const { questionBank, currentQuestion } = this.state;
+    if (currentQuestion  + 1 < questionBank.length) {
+      this.setState((prevState) => ({
+        currentQuestion: prevState .currentQuestion + 1,
+        selectedOption: "",
+      }));
+    } else{
+      this.setState({
+        quizEnd: true
+      });
+    }
+  }
+
+  checkAnswer = () => {
+    const {questionBank, currentQuestion, selectedOption, score} = this.state;
+    if (selectedOption === questionBank[currentQuestion].answer) {
+      continue
+    }
+  }
+
+  render() {
+    const {questionBank, currentQuestion, selectedOption, score, quizEnd} =
+      this.state;
+  
+    return (
+      <div className="App">
+        <h1>QUIZ APP</h1>
+        {/* умова ? true : false -------- тернарний оператор*/}
+        {!quizEnd ? (
+          <Question 
+            question ={} 
+            selectedOption ={}
+            onOptionChange ={}
+            onSunmit ={}
+          />
+        ) : ( 
+          <Score/>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Question.js
+++ src/Components/Question.js
@@ -3,13 +3,22 @@ import Options from "./Options";
 
 class Question extends Component {
     render () {
-        const {}= this.props;
+        const {question, selectedOptions, onOptionChange, onSubmit}= this.props;
 
         return(
             <div className="">
-                <h3></h3>
-                <h5></h5>
-                <form></form>
+                <h3>Question {question.id}</h3>
+                <h5 className="mt-2">{question.question}</h5>
+                <form onSubmit={onSubmit} className="">
+                    <Options
+                        options={question.question}
+                        selectedOptions={selectedOptions}
+                        onOptionChange={onOptionChange}
+                    />
+                    <button type="submit" className="">
+                        SUBMIT
+                    </button>
+                </form>
             </div>
         )
     }
